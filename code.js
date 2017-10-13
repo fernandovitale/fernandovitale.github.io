0eb47e34abdd5734868e7ae93c16ddaf2301f173
fernandovitale <32701688+fernandovitale@users.noreply.github.com>
2017-10-13 11:30:13 +0200

    Add files via upload

---
+++ code.js
@@ -0,0 +1,32 @@
+console.log("Hello Fernano"); 
+
+
+//alert("Hi dear Student");
+
+
+console.log(0.2345*1000);
+console.log(5+9);
+console.log(7/8);
+console.log(Math.random());
+
+document.getElementById('pop').innerHTML = "I put text here!"
+
+if (5>4) {
+	console.log("it is true")
+}
+
+else {
+	console.log("it is false")
+}
+
+
+function printApples(unknown) {
+	while (unknown<15){
+		console.log("I have an apple");
+		unknown+=1;
+
+	}
+
+}
+
+printApples(10);
